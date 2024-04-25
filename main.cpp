fdedab52a85407df402df0ef5328a1e824ff5d77
itssabahdry <isabal0088@gmail.com>
2024-04-25 00:44:00 -0700

    add class ostad to main.cpp file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
-#include <iostream>
+#include<iostream>
+#include<string>
+using namespace std;
+class ostad {
+private:
+    string user , pass;
+    string answer , question ;
+    string descriptiveQuestion[30] , testQuestion[30] , fouranswer[200];
+    int n = 0;
+    float totalPoint = 0;
+    float descriptivePoint[30], testPoint[30];
+    int i = 0 , j = 0 , correctAnswer[30];
+public:
+    ostad(string s1 , string s2) {
+        user = s1;
+        pass = s2;
+    }
+};
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
