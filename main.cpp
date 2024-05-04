96c2717f920a11625466ba3a98cdfbe6e690e9e5
Shayan <shayanfcfc@gmail.com>
2024-05-04 01:29:54 -0800

    delete constructor and add newExam , displayExam functions.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+
 #include<iostream>
 #include<string>
 using namespace std;
@@ -11,58 +12,73 @@ private:
     float descriptivePoint[30], testPoint[30];
     int i = 0 , j = 0 , correctAnswer[30];
 public:
-    ostad(string s1 , string s2) {
-        user = s1;
-        pass = s2;
-    }
+    
     void Exam() {
-        if(isTrueForOstad()) {
-            while(totalPoint != 20) {
-                cout << "Test or Descriptive ?";
-                cin >> answer;
-                if(answer == "Descriptive" || answer == "descriptive") {
-                    cin >> ws;
-                    getline(cin , question);
-                    descriptiveQuestion[i] = question;
-                    cin >> ws;
-                    cout << "Score for this question" << i + 1 << " : ";
+        while(totalPoint != 20) {
+            cout << "Test or Descriptive ?";
+            cin >> answer;
+            if(answer == "Descriptive" || answer == "descriptive") {
+                cin >> ws;
+                getline(cin , question);
+                descriptiveQuestion[i] = question;
+                cin >> ws;
+                cout << "Score for this question" << i + 1 << " : ";
+                cin >> descriptivePoint[i];
+                while((descriptivePoint[i] + totalPoint) > 20) {
+                    cout << "Score for this exam is more than 20!" << endl
+                         << "your total score is " << totalPoint << " . " << endl
+                         << "you can use up to " << 20 - totalPoint << " no more than this is allowed." << endl;
                     cin >> descriptivePoint[i];
-                    while((descriptivePoint[i] + totalPoint) > 20) {
-                        cout << "Score for this exam is more than 20!" << endl
-                        << "your total score is " << totalPoint << " . " << endl
-                        << "you can use up to " << 20 - totalPoint << " no more than this is allowed." << endl;
-                        cin >> descriptivePoint[i];
-                    }
-                    totalPoint += descriptivePoint[i];
-                    i++;
-                } else if(answer == "test" || answer == "Test") {
-                    cin >> ws;
-                    getline(cin , ask);
-                    testQuestion[j] = ask;
-                    for(int j = 0 + n; j < 4 + n ; j++) {
-                        getline(cin , fouranswer[j + n]);
-                    }
-                    cout << "correct answer is : (please enter the number --> (1,2,,3,4))";
-                    cin >> correctAnswer[j];
-                    n += 4;
-                    cout << "score for this question " << j + 1 << " : " << endl;
-                    cin >> testPoint[j];
-                    while((testPoint[j] + totalPoint) > 20) {
-                        cout << "score for this Exam is more than 20!" << endl <<
-                        "your total score is " << totalPoint << " . " << endl <<
-                        "you can use up to " << 20 - totalPoint << " ,no more than this is allowed. "
-                        << endl;
-                        cin >> testPoint[j];
-                    }
-                    totalPoint += testPoint[j];
-                    j++;
                 }
+                totalPoint += descriptivePoint[i];
+                i++;
+            } else if(answer == "test" || answer == "Test") {
+                cin >> ws;
+                getline(cin , ask);
+                testQuestion[j] = ask;
+                for(int j = 0 + n; j < 4 + n ; j++) {
+                    getline(cin , fouranswer[j + n]);
+                }
+                cout << "correct answer is : (please enter the number --> (1,2,,3,4))";
+                cin >> correctAnswer[j];
+                n += 4;
+                cout << "score for this question " << j + 1 << " : " << endl;
+                cin >> testPoint[j];
+                while((testPoint[j] + totalPoint) > 20) {
+                    cout << "score for this Exam is more than 20!" << endl <<
+                         "your total score is " << totalPoint << " . " << endl <<
+                         "you can use up to " << 20 - totalPoint << " ,no more than this is allowed. "
+                         << endl;
+                    cin >> testPoint[j];
+                }
+                totalPoint += testPoint[j];
+                j++;
             }
         }
-
     }
-    bool isTrueForOstad() {
-        return (user == "LOTFI" && pass == "1234");
+    void newExam( string namelist){
+        examList [ nameNum ] = namelist ;
+        cout << "new exam list '" << namelist << "' created succesfuly :)." << endl;
+        nameNum ++;
+    }
+    void displayExam( int listNumber){
+        if ( listNumber >= 0 && listNumber < 10 ){
+            cout << "Exam list " << listNumber + 1 << " : " << examList [ listNumber ] << endl;
+            for (int i = 0; questionTest [ i ] != "\0"; ++i) {
+                int p = 0;
+                cout << questionTest [ i ] << " ( " << pointTest [ i ] << " point )" << endl;
+                for (int k = four; k < four + 4 ; ++k) {
+                    cout << abcd [ p ] << " ) " << fourAwnser[ k ] << endl;
+                    p++;
+                }
+                four +=4;
+            }
+            for (int i = 0; questionDescriptive [ i ] != "\0" ; ++i) {
+                cout << questionDescriptive [ i ] << " ( " << pointDescriptive [ i ] << " point )" << endl;
+            }
+
+        } else {
+            cout << "Invalid list number!" << endl;
+        }
     }
 };
-
